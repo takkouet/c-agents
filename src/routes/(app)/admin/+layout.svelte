bddf8e3dfca9f61607820633e7960ab3058c5ac6
Theo Pham <theopham@Theos-MacBook-Air.local>
2026-03-24 16:14:12 +0700

    [MISC] Clean up

--- src/routes/(app)/admin/+layout.svelte
+++ src/routes/(app)/admin/+layout.svelte
@@ -100,14 +100,6 @@
 								: 'text-gray-300 dark:text-gray-600 hover:text-gray-700 dark:hover:text-white'} transition select-none"
 							href="/admin/settings">{$i18n.t('Settings')}</a
 						>
-
-						<a
-							draggable="false"
-							class="min-w-fit p-1.5 {$page.url.pathname.includes('/admin/bookings')
-								? ''
-								: 'text-gray-300 dark:text-gray-600 hover:text-gray-700 dark:hover:text-white'} transition select-none"
-							href="/admin/bookings">Bookings</a
-						>
 					</div>
 				</div>
 			</div>
--- src/routes/(app)/admin/bookings/+page.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-	import Bookings from '$lib/components/admin/Bookings.svelte';
-</script>
-
-<Bookings />
